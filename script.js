269e932618e7aa17a46671ea141f3cd6d6fa2083
Developer9719 <justin.bat19@gmail.com>
2025-09-01 20:31:22 -0400

    Basic Functionality Done

---
+++ script.js
@@ -0,0 +1,49 @@
+const books = []; // Array of book objects
+
+function Book(title, author) { // Book object constructor
+    // Uses the variable passed in as the instance variable
+    id = crypto.randomUUID(); // Sets random ID 
+    this.title = title,
+    this.author = author
+}
+
+function addBookToArray(name, author) {
+    /**
+     * Takes in arguments
+     */
+    this.name = name;
+    this.author = author;
+    /** Create a book object from the arguments
+     * - Each book needs a unique ID (crypto.randomUUID()) 
+     */
+    const book = new Book(name, author);
+    /** 
+     * Store the new object in the array
+     */
+    books.push(book);
+}
+
+addBookToArray('Lost City of Faar', 'DJ MacHale');
+addBookToArray('Merchant of Death', 'DJ MacHale');
+addBookToArray('The Reality Bug', 'DJ MacHale');
+addBookToArray('Sylo', 'DJ MacHale');
+addBookToArray('Black Water', 'DJ MacHale');
+
+
+// Display books 
+const displayElement = document.getElementsByClassName('currentInventory')[0];
+// The [0] selects the first element in the HTML Collection
+
+function displayBooks(book) {
+    console.log(book.name);
+    displayElement.insertAdjacentHTML("beforeend",`<div class="book">${book.title} 
+        by ${book.author}</div>`);
+}
+
+books.forEach(displayBooks);
+// books.foreach(displayBooks()); - Paases the result of the displayBooks 
+// function and since it doesnt return anything the result is always
+// undefined
+
+// books.forEach(displayBooks) - Passes a function reference, therefore 
+// telling the line to run the function
