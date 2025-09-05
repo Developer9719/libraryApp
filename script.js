bb2655d19c867b59813a80d25424178953c36fc1
Developer9719 <justin.bat19@gmail.com>
2025-09-05 10:29:49 -0400

    Add a new book from GUI is working

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const books = []; // Array of book objects
+const displayElement = document.getElementsByTagName('tbody')[0];
+// The [0] selects the first element in the HTML Collection
 
 function Book(title, author) { // Book object constructor
     // Uses the variable passed in as the instance variable
@@ -21,6 +23,7 @@ function addBookToArray(name, author) {
      * Store the new object in the array
      */
     books.push(book);
+    displayBooks(book);
 }
 
 addBookToArray('Lost City of Faar', 'DJ MacHale');
@@ -28,11 +31,6 @@ addBookToArray('Merchant of Death', 'DJ MacHale');
 addBookToArray('The Reality Bug', 'DJ MacHale');
 addBookToArray('Sylo', 'DJ MacHale');
 addBookToArray('Black Water', 'DJ MacHale');
-
-
-// Display books 
-const displayElement = document.getElementsByTagName('tbody')[0];
-// The [0] selects the first element in the HTML Collection
 
 function displayBooks(book) {
     console.log(book.name);
@@ -42,7 +40,6 @@ function displayBooks(book) {
 
 }
 
-books.forEach(displayBooks);
 // books.foreach(displayBooks()); - Paases the result of the displayBooks 
 // function and since it doesnt return anything the result is always
 // undefined
@@ -50,3 +47,22 @@ books.forEach(displayBooks);
 // books.forEach(displayBooks) - Passes a function reference, therefore 
 // telling the line to run the function
 
+// Local Form Processing
+const form = document.getElementById('newBookForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  // Get Form Data
+    const bookTitle = document.getElementById('title').value;
+    const bookAuthor = document.getElementById('author').value;
+
+    // Form Validation
+    if(!bookTitle) {
+        alert('Please enter a book title');
+        return;
+    } else if(!bookAuthor) {
+        alert('Please enter a book author');
+        return;
+    }
+
+  addBookToArray(bookTitle, bookAuthor);
+});
